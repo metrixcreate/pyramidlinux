063b16bb15e7eba35f5177fde4a6ab48dd68b1af
ken <ken@f5386d9a-215a-11de-9909-f37ff7dbbc12>
2006-09-07 18:47:51 +0000

    Added script to build from SVN

---
+++ Pyramid/build/pyramid-export.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+#Shell script to export Pyramid from SVN and make a tarball for PXE install
+#
+#We assume PWD is where you want all of this to take place
+DATE=$(date +%T-%D | sed -e "s/\//-/g")
+FILENAME=pyramid-$DATE.tgz
+HOST=localhost
+REPO="MetrixSvn/Pyramid/dist/"
+EXPORTCMD="svn export svn://$HOST/$REPO" 
+WHOAREYOU=$(whoami)
+if [ $WHOAREYOU != root ]; then
+echo "You need to run this as root, in order to properly set permissions in the tarball distro"
+exit
+fi
+
+mkdir work
+cd work
+$EXPORTCMD
+cd dist
+#Insert stuff to fix baseline permissions here
+chown -R 0:0 etc
+chown -R 0:0 bin
+chown -R 0:0 boot
+chown -R 0:50 home
+chown -R 0:0 lib
+chown -R 0:0 lost+found
+chown -R 0:0 mnt
+chown -R 0:0 ro
+chown -R 0:0 sbin
+chown -R 0:0 tmp
+chown -R 0:0 usr
+chown -R 0:0 var
+#next round of permission fixing
+chmod 0440 etc/sudoers
+
+
+tar -cvpf ../$FILENAME *
+cd ..
