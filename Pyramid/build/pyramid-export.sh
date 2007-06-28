7025564a28401db94b54c5ac34457454ce4dfa67
btm <btm@f5386d9a-215a-11de-9909-f37ff7dbbc12>
2007-06-28 05:04:59 +0000

    Adds fakeroot support to build script
    Creates etc/pyramid_version based on svn revision
    Names tar file based on svn revision
    Creates issue files with svn revision

--- Pyramid/build/pyramid-export.sh
+++ Pyramid/build/pyramid-export.sh
@@ -2,23 +2,27 @@
 #Shell script to export Pyramid from SVN and make a tarball for PXE install
 #
 #We assume PWD is where you want all of this to take place
-#
-#
-DATE=$(date +%T-%D | sed -e "s/\//-/g")
-FILENAME=pyramid-$DATE.tgz
+FILEHEAD=pyramid
 PROTO=http
 HOST=pyramid.metrix.net
 REPO="svn/Pyramid/dist/"
-EXPORTCMD="svn export $PROTO://$HOST/$REPO" 
+EXPORTCMD="svn export $PROTO://$HOST/$REPO | tee export.log" 
 WHOAREYOU=$(whoami)
-if [ $WHOAREYOU != root ]; then
-echo "You need to run this as root, in order to properly set permissions in the tarball distro"
+FAKEROOT=/usr/bin/fakeroot
+
+if [ $WHOAREYOU != root ] && [ ! -x $FAKEROOT ] ; then
+echo "You need to run this as root or have fakeroot installed in order to properly set permissions in the tarball distro"
 exit
 fi
 
+cat > pyramid-work.sh << EOF
+#!/bin/bash
 mkdir work
 cd work
 $EXPORTCMD
+if [ -e export.log ] ; then
+  sed -n 's/^Exported revision \([0-9]*\)./svn-\1/p' export.log > dist/etc/pyramid_version
+fi
 cd dist
 #Insert stuff to fix baseline permissions here
 chown -R 0:0 etc
@@ -33,13 +37,28 @@ chown -R 0:0 sbin
 chown -R 0:0 tmp
 chown -R 0:0 usr
 chown -R 0:0 var
+chown -R 0:0 sys dev proc root rw
 chown -R 500:500 ro/kismet
 #next round of permission fixing
 chmod 0440 etc/sudoers
 chmod -R +x etc/init.d/
 chmod 777 tmp
 
+if [ -e ../export.log ] ; then
+  sed -n 's/^Exported revision \([0-9]*\)./Metrix Pyramid\/\\\\s  \\\\n \\\\l (svn-\1)\n/p' ../export.log > ro/etc/issue
+  sed -n 's/^Exported revision \([0-9]*\)./Metrix Pyramid\/%s %h (svn-\1)/p' ../export.log > ro/etc/issue.net
+fi
 
-
-tar -cvpf ../$FILENAME *
+tar -cvpf ../build.tar *
 cd ..
+EOF
+chmod 755 pyramid-work.sh
+
+if [ -x $FAKEROOT ] ; then
+	$FAKEROOT "./pyramid-work.sh"
+else
+	./pyramid-work.sh
+fi
+
+mv work/build.tar $FILEHEAD-`cat work/dist/etc/pyramid_version`.tar
+rm pyramid-work.sh
